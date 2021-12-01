2cca10703e5277a907a858b64b47e38fb5c3f844
Daniell Algar <daniell.algar@gmail.com>
2021-12-01 17:27:47 +0100

    Day 1 task 2

--- app/src/test/java/com/algar/adventofcode2021/Day01.kt
+++ app/src/test/java/com/algar/adventofcode2021/Day01.kt
@@ -8,14 +8,24 @@ class Day01 {
 
     @Test
     fun `task 1 - how many measurements are larger than the previous`() {
-        val input = getInput()
-
-        val diff = input.drop(1).mapIndexed { index, depth ->
-            depth - input[index]
-        }
-        val result = diff.count { it > 0 }
-        println("Task 1 - Number of positive derivatives: $result")
+        val result = diff(getInput()).count { it > 0 }
+        println("Task 1 - Number of positive derivatives: $result.")
     }
+
+    @Test
+    fun `task 2 - like task 1 but with a 3 measurement sliding window`() {
+        val result = diff(getSlidingWindow(getInput())).count { it > 0 }
+        println("Task 2 - Number of positive derivatives: $result.")
+    }
+
+    private fun diff(input: List<Int>): List<Int> = input.drop(1).mapIndexed { index, depth ->
+        depth - input[index]
+    }
+
+    private fun getSlidingWindow(input: List<Int>): List<Int> =
+        (0..input.size - 3).mapIndexed { index, _ ->
+            input[index] + input[index + 1] + input[index + 2]
+        }
 
     private fun getInput(): List<Int> {
         val output = arrayListOf<Int>()
