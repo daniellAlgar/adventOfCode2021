3e58fcc7355c777bfa411b1dc1947d81b5065183
Daniell Algar <daniell.algar@gmail.com>
2021-12-03 11:39:40 +0100

    Day 3 task 2

--- app/src/test/java/com/algar/adventofcode2021/Day03.kt
+++ app/src/test/java/com/algar/adventofcode2021/Day03.kt
@@ -19,6 +19,30 @@ class Day03 {
         println("Epsilon rate: $epsilonRate")
     }
 
+    @Test
+    fun `task 2 - life support rating`() {
+        val input = getInput()
+        var oxygenRating = input
+        var co2Rating = input
+
+        var currentBit = 0
+        while (oxygenRating.size > 1) {
+            val keepFlag =
+                oxygenRating.count { it[currentBit] } >= oxygenRating.count { !it[currentBit] }
+            oxygenRating = oxygenRating.filter { it[currentBit] == keepFlag }
+            currentBit++
+        }
+
+        currentBit = 0
+        while (co2Rating.size > 1) {
+            val keepFlag = co2Rating.count { it[currentBit] } < co2Rating.count { !it[currentBit] }
+            co2Rating = co2Rating.filter { it[currentBit] == keepFlag }
+            currentBit++
+        }
+
+        println("Oxygen rating: $oxygenRating, CO2 rating: $co2Rating")
+    }
+
     private fun getInput(): List<List<Boolean>> {
         val output = arrayListOf<List<Boolean>>()
         val inputStream = javaClass.getResourceAsStream("day03.txt")
